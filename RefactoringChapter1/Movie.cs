adecb27a1170ac1750e3e9a241c6b1808112e944
Michael Welch <michaelgwelch@gmail.com>
2012-02-06 14:57:59 -0600

    First checkin

---
+++ RefactoringChapter1/Movie.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace RefactoringChapter1
+{
+	public class Movie
+	{
+		public const int Childrens = 2;
+		public const int Regular = 0;
+		public const int NewRelease = 1;
+		
+		public Movie ()
+		{
+		}
+	}
+}
+
